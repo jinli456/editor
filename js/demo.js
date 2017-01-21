7db27b00f9aa6be103a8c29ac37d0480d5a52837
jinli456 <1484225473@qq.com>
2017-01-21 09:19:15 +0800

    '1'

---
+++ js/demo.js
@@ -0,0 +1,211 @@
+$(document).ready(function(){
+    // Full featured editor
+    $('#editor1,#editor2,#editor3').each( function(index, element)
+    {
+        $(element).wysiwyg({
+            classes: 'some-more-classes',
+            // 'selection'|'top'|'top-selection'|'bottom'|'bottom-selection'
+            toolbar: index == 0 ? 'top-selection' : (index == 1 ? 'bottom' : 'selection'),
+            buttons: {
+                // Dummy-HTML-Plugin
+                dummybutton1: index != 1 ? false : {
+                    html: $('<input id="submit" type="button" value="bold" />').click(function(){
+                                // We simply make 'bold'
+                                if( $(element).wysiwyg('shell').getSelectedHTML() )
+                                    $(element).wysiwyg('shell').bold();
+                                else
+                                    alert( 'Please selection some text' );
+                            }),
+                    //showstatic: true,    // wanted on the toolbar
+                    showselection: false    // wanted on selection
+                },
+                // Dummy-Button-Plugin
+                dummybutton2: index != 1 ? false : {
+                    title: 'Dummy',
+                    image: '\uf1e7',
+                    click: function( $button ) {
+                            alert('Do something');
+                           },
+                    //showstatic: true,    // wanted on the toolbar
+                    showselection: false    // wanted on selection
+                },
+                insertimage: {
+                    title: 'Insert image',
+                    image: '\uf030', // <img src="path/to/image.png" width="16" height="16" alt="" />
+                    //showstatic: true,    // wanted on the toolbar
+                    showselection: false    // wanted on selection
+                },
+                // Fontname plugin
+                fontname: index == 1 ? false : {
+                    title: 'Font',
+                    image: '\uf031', // <img src="path/to/image.png" width="16" height="16" alt="" />
+                    popup: function( $popup, $button ) {
+                            var list_fontnames = {
+                                    // Name : Font
+                                    'Arial, Helvetica' : 'Arial,Helvetica',
+                                    'Verdana'          : 'Verdana,Geneva',
+                                    'Georgia'          : 'Georgia',
+                                    'Courier New'      : 'Courier New,Courier',
+                                    'Times New Roman'  : 'Times New Roman,Times'
+                                };
+                            var $list = $('<div/>').addClass('wysiwyg-toolbar-list')
+                                                   .attr('unselectable','on');
+                            $.each( list_fontnames, function( name, font ){
+                                var $link = $('<a/>').attr('href','#')
+                                                    .css( 'font-family', font )
+                                                    .html( name )
+                                                    .click(function(event){
+                                                        $(element).wysiwyg('shell').fontName(font).closePopup();
+                                                        // prevent link-href-#
+                                                        event.stopPropagation();
+                                                        event.preventDefault();
+                                                        return false;
+                                                    });
+                                $list.append( $link );
+                            });
+                            $popup.append( $list );
+                           },
+                    //showstatic: true,    // wanted on the toolbar
+                    showselection: index == 0 ? true : false    // wanted on selection
+                },
+                // Fontsize plugin
+                fontsize: index == 1 ? false : {
+                    title: 'Size',
+                    image: '\uf034', // <img src="path/to/image.png" width="16" height="16" alt="" />
+                    popup: function( $popup, $button ) {
+                            var list_fontsizes = {
+                                // Name : Size
+                                'Huge'    : 7,
+                                'Larger'  : 6,
+                                'Large'   : 5,
+                                'Normal'  : 4,
+                                'Small'   : 3,
+                                'Smaller' : 2,
+                                'Tiny'    : 1
+                            };
+                            var $list = $('<div/>').addClass('wysiwyg-toolbar-list')
+                                                   .attr('unselectable','on');
+                            $.each( list_fontsizes, function( name, size ){
+                                var $link = $('<a/>').attr('href','#')
+                                                    .css( 'font-size', (8 + (size * 3)) + 'px' )
+                                                    .html( name )
+                                                    .click(function(event){
+                                                        $(element).wysiwyg('shell').fontSize(size).closePopup();
+                                                        // prevent link-href-#
+                                                        event.stopPropagation();
+                                                        event.preventDefault();
+                                                        return false;
+                                                    });
+                                $list.append( $link );
+                            });
+                            $popup.append( $list );
+                           }
+                    //showstatic: true,    // wanted on the toolbar
+                    //showselection: true    // wanted on selection
+                },
+                // Header plugin
+                header: index != 1 ? false : {
+                    title: 'Header',
+                    image: '\uf1dc', // <img src="path/to/image.png" width="16" height="16" alt="" />
+                    popup: function( $popup, $button ) {
+                            var list_headers = {
+                                    // Name : Font
+                                    'Header 1'     : '<h1>',
+                                    'Header 2'     : '<h2>',
+                                    'Header 3'     : '<h3>',
+                                    'Header 4'     : '<h4>',
+                                    'Header 5'     : '<h5>',
+                                    'Header 6'     : '<h6>',
+                                    'Preformatted' : '<pre>'
+                                };
+                            var $list = $('<div/>').addClass('wysiwyg-toolbar-list')
+                                                   .attr('unselectable','on');
+                            $.each( list_headers, function( name, format ){
+                                var $link = $('<a/>').attr('href','#')
+                                                     .css( 'font-family', format )
+                                                     .html( name )
+                                                     .click(function(event){
+                                                        $(element).wysiwyg('shell').format(format).closePopup();
+                                                        // prevent link-href-#
+                                                        event.stopPropagation();
+                                                        event.preventDefault();
+                                                        return false;
+                                                    });
+                                $list.append( $link );
+                            });
+                            $popup.append( $list );
+                           }
+                    //showstatic: true,    // wanted on the toolbar
+                    //showselection: false    // wanted on selection
+                },
+                bold: {
+                    title: 'Bold (Ctrl+B)',
+                    image: '\uf032', // <img src="path/to/image.png" width="16" height="16" alt="" />
+                    hotkey: 'b'
+                },
+                forecolor: {
+                    title: 'Text color',
+                    image: '\uf1fc' // <img src="path/to/image.png" width="16" height="16" alt="" />
+                },
+            },
+            // Submit-Button
+            submit: {
+                title: 'Submit',
+                image: '\uf00c' // <img src="path/to/image.png" width="16" height="16" alt="" />
+            },
+            // Other properties
+            dropfileclick: 'Drop image or click',
+            placeholderUrl: 'www.example.com',
+            maxImageSize: [600,200]
+        })
+        .change(function(){
+            if( typeof console != 'undefined' )
+                console.log( 'change' );
+        })
+        .focus(function(){
+            if( typeof console != 'undefined' )
+                console.log( 'focus' );
+        })
+        .blur(function(){
+            if( typeof console != 'undefined' )
+                console.log( 'blur' );
+        });
+    });
+
+    // Demo-Buttons
+    $('#editor3-bold').click(function(){
+        $('#editor3').wysiwyg('shell').bold();
+        return false;
+    });
+    $('#editor3-red').click(function(){
+        $('#editor3').wysiwyg('shell').highlight('#ff0000');
+        return false;
+    });
+    $('#editor3-sethtml').click(function(){
+        $('#editor3').wysiwyg('shell').setHTML('This is the new text.');
+        return false;
+    });
+    $('#editor3-inserthtml').click(function(){
+        $('#editor3').wysiwyg('shell').insertHTML('Insert some text.');
+        return false;
+    });
+
+    // Raw editor
+    var option = {
+        element: $('#editor0').get(0),
+        onkeypress: function( code, character, shiftKey, altKey, ctrlKey, metaKey ) {
+                        if( typeof console != 'undefined' )
+                            console.log( 'RAW: '+character+' key pressed' );
+                    },
+        onselection: function( collapsed, rect, nodes, rightclick ) {
+                        if( typeof console != 'undefined' && rect )
+                            console.log( 'RAW: selection rect('+rect.left+','+rect.top+','+rect.width+','+rect.height+'), '+nodes.length+' nodes' );
+                    },
+        onplaceholder: function( visible ) {
+                        if( typeof console != 'undefined' )
+                            console.log( 'RAW: placeholder ' + (visible ? 'visible' : 'hidden') );
+                    }
+    };
+    var wysiwygeditor = wysiwyg( option );
+    //wysiwygeditor.setHTML( '<html>' );
+});
